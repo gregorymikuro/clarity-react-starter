267bdf0c0bc01252b565c9f00ebcf11420d22162
Franklin Shera <fshera96@gmail.com>
2025-08-24 11:48:46 +0300

    update

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import type { RouteConfig } from "@react-router/dev/routes";
+import { remixRoutesOptionAdapter } from "@react-router/remix-routes-option-adapter";
+import { flatRoutes } from "remix-flat-routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default remixRoutesOptionAdapter((defineRoutes) => {
+  return flatRoutes("routes", defineRoutes, {
+    ignoredRouteFiles: [
+      "**/.*", // ignore dotfiles - files starting with .
+      "**/__*.*", // ignore files starting with (double underscore) __
+      "**/*.server.ts", // ignore server-only files - files ending with .server.ts
+      "**/*.client.ts", // ignore client-only files - files ending with .client.ts
+    ],
+  });
+}) satisfies RouteConfig;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,3 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,5 +6,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <h1>Hi Cousins!</h1>;
 }
