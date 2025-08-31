dabbeb09a1f67423c28bdf1fe1319e38e669ac1a
Franklin Shera <fshera96@gmail.com>
2025-08-31 22:30:59 +0300

    generate migrations

---
+++ drizzle/0000_colossal_loners.sql
@@ -0,0 +1,54 @@
+CREATE TABLE `accounts` (
+	`id` varchar(36) NOT NULL,
+	`account_id` text NOT NULL,
+	`provider_id` text NOT NULL,
+	`user_id` varchar(36) NOT NULL,
+	`access_token` text,
+	`refresh_token` text,
+	`id_token` text,
+	`access_token_expires_at` timestamp,
+	`refresh_token_expires_at` timestamp,
+	`scope` text,
+	`password` text,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `accounts_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `sessions` (
+	`id` varchar(36) NOT NULL,
+	`expires_at` timestamp NOT NULL,
+	`token` varchar(255) NOT NULL,
+	`ip_address` text,
+	`user_agent` text,
+	`user_id` varchar(36) NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `sessions_id` PRIMARY KEY(`id`),
+	CONSTRAINT `sessions_token_unique` UNIQUE(`token`)
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` varchar(36) NOT NULL,
+	`name` text NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`email_verified` boolean NOT NULL,
+	`image` text,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `users_id` PRIMARY KEY(`id`),
+	CONSTRAINT `users_email_unique` UNIQUE(`email`)
+);
+--> statement-breakpoint
+CREATE TABLE `verifications` (
+	`id` varchar(36) NOT NULL,
+	`identifier` text NOT NULL,
+	`value` text NOT NULL,
+	`expires_at` timestamp NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `verifications_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+ALTER TABLE `accounts` ADD CONSTRAINT `accounts_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;
